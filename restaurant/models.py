bf41b935a5d3eeed42d443cbe52bfa7773969a1d
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-05 14:32:01 +0000

    Add Restaurant database  model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    opening_time = models.TimeField(auto_now=False, auto_now_add=False)
+    closing_time = models.TimeField(auto_now=False, auto_now_add=False)
+    menu = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
