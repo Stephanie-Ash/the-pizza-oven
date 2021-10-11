720ca46b1b8f266f197f9f50415fc91074cfe66a
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-11 07:52:40 +0000

    Add index view and url

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Create your views here.
+
+def index(request):
+    """
+    A view to return the homepage with restaurant information populated
+    """
+    restaurant = Restaurant.objects.get(name="The Pizza Oven")
+    context = {
+        'restaurant': restaurant,
+    }
+
+    return render(request, 'restaurant/index.html', context)
