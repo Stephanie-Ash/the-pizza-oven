986dd6114a1677ce77e0b961aa84b84e1e77b177
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-20 14:48:20 +0000

    Add function to check for avaialable tables for booking

--- bookings/check_availability.py
+++ bookings/check_availability.py
@@ -1,5 +1,7 @@
 """ Set up booking slots and check for available tables """
 from datetime import datetime, date, timedelta
+from itertools import combinations, chain
+from restaurant.models import Table
 
 
 def create_booking_slots(opening_time, closing_time):
@@ -18,3 +20,34 @@ def create_booking_slots(opening_time, closing_time):
         current_slot += slot_interval
 
     return [(slot.time(), slot.strftime('%H:%M')) for slot in booking_slots]
+
+
+def find_tables(date, time, end_time, party_size):
+    """ Search for available tables on the date and time of the booking """
+
+    # Exclude any tables whose bookings overlap
+    # the start time of the required booking
+    check1 = Table.objects.exclude(
+        bookings__date=date,
+        bookings__time__lt=time,
+        bookings__end_time__gt=time)
+
+    # Exclude any tables whose bookings overlap
+    # the end time of the required booking
+    check2 = check1.exclude(
+        bookings__date=date,
+        bookings__time__lt=end_time,
+        bookings__end_time__gt=end_time)
+
+    # Exclude any tables whose bookings span the required booking
+    # booking length is fixed at 2 hours so there will be no bookings
+    # within the time range
+    available_tables = check2.exclude(
+        bookings__date=date,
+        bookings__time__lte=time,
+        bookings__end_time__gte=end_time)
+
+    # If there are any tables left after the checks
+    # we need to select one or more for the booking
+    if available_tables:
+        return select_single_table(available_tables, party_size)
