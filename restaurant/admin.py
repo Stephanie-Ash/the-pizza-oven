23cfecaef466283d2f7deb9f7a5f4d1ceeea03ba
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-20 12:41:36 +0000

    Fix error in Table Admin class name

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,14 +1,17 @@
+""" Admin set-up for the restaurant app"""
 from django.contrib import admin
 from .models import Restaurant, Table
 
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
+    """ Admin options for the Restaurant model """
     list_display = ('name', 'opening_time', 'closing_time')
     search_fields = ('name',)
 
 
 @admin.register(Table)
-class RestaurantAdmin(admin.ModelAdmin):
+class TableAdmin(admin.ModelAdmin):
+    """ Admin options for the Table model """
     list_display = ('restaurant', 'size')
     ordering = ('size',)
