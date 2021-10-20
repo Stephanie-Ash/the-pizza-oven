caa229732fceeb9932d3526a6f58c40e0ff10188
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-20 15:13:21 +0000

    Add function to combine tables to fulfil booking

--- bookings/check_availability.py
+++ bookings/check_availability.py
@@ -51,3 +51,67 @@ def find_tables(date, time, end_time, party_size):
     # we need to select one or more for the booking
     if available_tables:
         return select_single_table(available_tables, party_size)
+
+
+def select_single_table(tables, party_size):
+    """
+    Check the available tables and see if there is one
+    big enough for the required party size
+    """
+
+    # It is preferred to fulfil the booking will a single table
+    # so check for this first
+    leftover = 0
+    selected_table = ''
+    for table in tables:
+        # first check for table of exact size
+        if table.size == party_size:
+            selected_table = table
+            return selected_table
+        # next try to find the smallest possible table
+        elif table.size > party_size:
+            spaces_left = table.size - party_size
+            if leftover:
+                if spaces_left < leftover:
+                    leftover = spaces_left
+                    selected_table = table
+            else:
+                leftover = spaces_left
+                selected_table = table
+
+    if selected_table:
+        return selected_table
+    else:
+        # if still no table has been found,
+        # see if we can combine tables to fit the party size
+        return combine_tables(tables, party_size)
+
+
+def combine_tables(tables, party_size):
+    """ Combine available tables to fit the party size if possible"""
+
+    # With tables only 2 or 4 person in size and party size maximum 8
+    # we will only need to combine up to 4 tables
+    combos2 = combinations(tables, 2)
+    combos3 = combinations(tables, 3)
+    combos4 = combinations(tables, 4)
+    leftover = 0
+    combined_tables = ''
+    for combo in chain(combos2, combos3, combos4):
+        combined_size = sum([table.size for table in combo])
+        if combined_size == party_size:
+            combined_tables = list(combo)
+            return combined_tables
+        elif combined_size > party_size:
+            spaces_left = combined_size - party_size
+            if leftover:
+                if spaces_left < leftover:
+                    leftover = spaces_left
+                    combined_tables = list(combo)
+            else:
+                leftover = spaces_left
+                combined_tables = list(combo)
+
+    if combined_tables:
+        return combined_tables
+    # if we have not returned by now there are no tables for the booking
