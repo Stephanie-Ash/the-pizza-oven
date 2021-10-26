fd18baa7a2dfe0a89f649dff7f1ef251e4186d1b
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-26 10:36:52 +0000

    Set the name as readonly and disable delete in the restaurant admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -6,8 +6,12 @@ from .models import Restaurant, Table
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     """ Admin options for the Restaurant model """
+    readonly_fields = ('name',)
     list_display = ('name', 'opening_time', 'closing_time')
     search_fields = ('name',)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Table)
