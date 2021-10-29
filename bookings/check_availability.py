44a03a500a0c9c30038628fc736ba74600c711c9
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-10-29 07:36:59 +0000

    Revise table check when updating bookings to ignore current booking

--- bookings/check_availability.py
+++ bookings/check_availability.py
@@ -25,27 +25,42 @@ def create_booking_slots(opening_time, closing_time):
 
 def find_tables(date, time, end_time, party_size, booking_id):
     """ Search for available tables on the date and time of the booking """
-
+    # Exclude any tables whose bookings overlap
+    # the start time of the required booking
     if booking_id:
         check1 = Table.objects.exclude(
-            bookings__in=Booking.objects.exclude(id=booking_id).filter(date=date, time__lt=time, end_time__gt=time))
+            bookings__in=Booking.objects.exclude(id=booking_id).filter(
+                date=date, time__lt=time, end_time__gt=time))
     else:
-        check1 = Table.objects.exclude(bookings__date=date, bookings__time__lt=time, bookings__end_time__gt=time)
+        check1 = Table.objects.exclude(
+            bookings__date=date,
+            bookings__time__lt=time,
+            bookings__end_time__gt=time)
 
     # Exclude any tables whose bookings overlap
     # the end time of the required booking
-    check2 = check1.exclude(
-        bookings__date=date,
-        bookings__time__lt=end_time,
-        bookings__end_time__gt=end_time)
+    if booking_id:
+        check2 = check1.exclude(
+            bookings__in=Booking.objects.exclude(id=booking_id).filter(
+                date=date, time__lt=end_time, end_time__gt=end_time))
+    else:
+        check2 = check1.exclude(
+            bookings__date=date,
+            bookings__time__lt=end_time,
+            bookings__end_time__gt=end_time)
 
     # Exclude any tables whose bookings span the required booking
     # booking length is fixed at 2 hours so there will be no bookings
     # within the time range
-    available_tables = check2.exclude(
-        bookings__date=date,
-        bookings__time__lte=time,
-        bookings__end_time__gte=end_time)
+    if booking_id:
+        available_tables = check2.exclude(
+            bookings__in=Booking.objects.exclude(id=booking_id).filter(
+                date=date, time__lte=time, end_time__gte=end_time))
+    else:
+        available_tables = check2.exclude(
+            bookings__date=date,
+            bookings__time__lte=time,
+            bookings__end_time__gte=end_time)
 
     # If there are any tables left after the checks
     # we need to select one or more for the booking
