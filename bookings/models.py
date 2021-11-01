b83b96aff2fe8140bfe8b19f141ecee465960c7d
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-01 15:11:29 +0000

    Add updated field to bookings model

--- bookings/models.py
+++ bookings/models.py
@@ -40,6 +40,7 @@ class Booking(models.Model):
     special_requirements = models.TextField(blank=True)
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
+    updated = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-date', '-time']
