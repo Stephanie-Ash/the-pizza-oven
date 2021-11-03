bd56448697bff226356706f325e8f219020562ca
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-03 08:17:35 +0000

    Further configure the bookings admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,8 +6,13 @@ from .models import Booking
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     """ Admin options for the Booking model """
-    list_display = ('date', 'time', 'end_time', 'party_size')
+    fields = ('date', 'time', 'party_size', 'tables', 'table_numbers',
+              'customer', 'name', 'email', 'phone_number',
+              'special_requirements', 'updated')
+    list_display = ('name', 'email', 'date', 'time', 'party_size')
     readonly_fields = ('date', 'time', 'party_size', 'tables')
+    search_fields = ['name']
+    list_filter = ('date', 'party_size', 'updated')
 
     def has_add_permission(self, request):
         return False
