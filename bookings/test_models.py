367fb46caf43c938033faa155e2f52d3ed92746d
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-08 14:49:55 +0000

    Add test to check the Booking model string method

--- bookings/test_models.py
+++ bookings/test_models.py
@@ -1,3 +1,12 @@
+import datetime
 from django.test import TestCase
+from .models import Booking
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_booking_string_method_includes_date_and_party_suze(self):
+        booking = Booking.objects.create(
+            date=datetime.date(2021, 11, 8), time=datetime.time(12, 00),
+            party_size=4, name='Test Name', email='test@email.com',
+            phone_number='01234567890')
+        self.assertEqual(str(booking), 'A table of 4 on 08-11-2021')
