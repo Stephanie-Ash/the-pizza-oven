e4f6e6d190b19555932ac64734ead575c50a3678
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-08 13:59:46 +0000

    Add setUp method to restaurant test_models

--- restaurant/test_models.py
+++ restaurant/test_models.py
@@ -1,3 +1,4 @@
+""" Testcases for the restaurant app models. """
 import datetime
 from django.test import TestCase
 from django.core.exceptions import ValidationError
@@ -5,18 +6,23 @@ from .models import Restaurant, Table
 
 
 class TestModels(TestCase):
+    """ Tests for the models. """
+    def setUp(self):
+        self.restaurant = Restaurant.objects.create(name='Test Restaurant')
+
     def test_restaurant_string_method_returns_name(self):
-        restaurant = Restaurant.objects.create(name='Test Restaurant')
-        self.assertEqual(str(restaurant), 'Test Restaurant')
+        """ Test the Restaurant model string method. """
+        self.assertEqual(str(self.restaurant), 'Test Restaurant')
 
     def test_table_string_method_returns_string_including_size(self):
-        restaurant = Restaurant.objects.create(name='Test Restaurant')
-        table = Table.objects.create(restaurant=restaurant, size=2)
+        """ Test the Table model string method. """
+        table = Table.objects.create(restaurant=self.restaurant, size=2)
         self.assertEqual(str(table), 'A table of 2 people size')
 
     def test_restaurant_closing_time_must_be_later_than_opening_time(self):
+        """ Test the restaurant model custom clean method. """
         open_time = datetime.time(11, 00, 00)
         close_time = datetime.time(10, 00, 00)
-        restaurant = Restaurant.objects.create(
-            name='Test Restaurant', opening_time=open_time, closing_time=close_time)
-        self.assertRaises(ValidationError, restaurant.clean)
+        self.restaurant.opening_time = open_time
+        self.restaurant.closing_time = close_time
+        self.assertRaises(ValidationError, self.restaurant.clean)
