5e340871cc1f17909a5ba53816445bda60578b85
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-08 11:16:35 +0000

    Create Test case to test the restaurant app index view

---
+++ restaurant/test_views.py
@@ -0,0 +1,14 @@
+""" Testcases for the restaurant app views. """
+from django.test import TestCase
+from .models import Restaurant
+
+
+class TestViews(TestCase):
+    """ Tests for the views. """
+
+    def test_get_index_page(self):
+        """ Test the index page view. """
+        Restaurant.objects.create(name='The Pizza Oven')
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'restaurant/index.html')
