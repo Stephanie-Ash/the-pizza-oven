dbc66b1b28a0c51b4606dcb4f0d957c487196f90
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-08 14:51:33 +0000

    Add test to check that a booking end time is added on save

--- bookings/test_models.py
+++ bookings/test_models.py
@@ -10,3 +10,10 @@ class TestModels(TestCase):
             party_size=4, name='Test Name', email='test@email.com',
             phone_number='01234567890')
         self.assertEqual(str(booking), 'A table of 4 on 08-11-2021')
+
+    def test_booking_end_time_generated_on_save(self):
+        booking = Booking.objects.create(
+            date=datetime.date(2021, 11, 8), time=datetime.time(12, 00),
+            party_size=4, name='Test Name', email='test@email.com',
+            phone_number='01234567890')
+        self.assertEqual(booking.end_time, datetime.time(14, 00))
