7deee50e6063e05584a035abc7f0545eacbdfdbd
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-08 11:41:07 +0000

    Add tests to text restaurant model string methods

--- restaurant/test_models.py
+++ restaurant/test_models.py
@@ -1,1 +1,13 @@
 from django.test import TestCase
+from .models import Restaurant, Table
+
+
+class TestModels(TestCase):
+    def test_restaurant_string_method_returns_name(self):
+        restaurant = Restaurant.objects.create(name='Test Restaurant')
+        self.assertEqual(str(restaurant), 'Test Restaurant')
+
+    def test_table_string_method_returns_string_including_size(self):
+        restaurant = Restaurant.objects.create(name='Test Restaurant')
+        table = Table.objects.create(restaurant=restaurant, size=2)
+        self.assertEqual(str(table), 'A table of 2 people size')
