6377545d84f80f2fb34d40981bf0f1695e203e60
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-09 09:04:30 +0000

    Test the get booking detail page view

--- bookings/test_views.py
+++ bookings/test_views.py
@@ -10,24 +10,26 @@ class TestViews(TestCase):
     """ Tests for the views. """
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_superuser('admin', 'admin@email.com', 'adminpassword')
+        self.user = User.objects.create_superuser(
+            'admin', 'admin@email.com', 'adminpassword')
+
+        self.restaurant = Restaurant.objects.create(name='The Pizza Oven')
+        self.table = Table.objects.create(restaurant=self.restaurant, size=2)
+        self.booking = Booking.objects.create(
+            name='Test Name', email='test@email.com',
+            phone_number='01234567890')
+        self.booking.tables.add(self.table)
 
     def test_get_make_booking_page(self):
         """ Test the get make booking page view. """
-        Restaurant.objects.create(name='The Pizza Oven')
         response = self.client.get('/bookings/make_booking')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bookings/make_booking.html')
 
     def test_get_booking_confirmed_page(self):
         """ Test the get booking confirmed page view. """
-        restaurant = Restaurant.objects.create(name='The Pizza Oven')
-        table = Table.objects.create(restaurant=restaurant, size=2)
-        booking = Booking.objects.create(
-            name='Test Name', email='test@email.com',
-            phone_number='01234567890')
-        booking.tables.add(table)
-        response = self.client.get(f'/bookings/booking_confirmed/{booking.id}')
+        response = self.client.get(
+            f'/bookings/booking_confirmed/{self.booking.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bookings/booking_confirmed.html')
 
@@ -37,3 +39,11 @@ class TestViews(TestCase):
         response = self.client.get('/bookings/manage_bookings')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bookings/manage_bookings.html')
+
+    def test_get_booking_detail_page(self):
+        """ Test the get booking detail page view. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(
+            f'/bookings/booking_detail/{self.booking.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/booking_detail.html')
