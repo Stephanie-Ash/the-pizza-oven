79b728de20bb2e9d710387e8988687c2736f124f
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-09 08:13:05 +0000

    Add test to test the get make booking page view

--- bookings/test_views.py
+++ bookings/test_views.py
@@ -1,3 +1,13 @@
+""" Testcases for the restaurant app views. """
 from django.test import TestCase
+from restaurant.models import Restaurant
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """ Tests for the views. """
+    def test_get_make_booking_page(self):
+        """ Test the get make booking page view. """
+        Restaurant.objects.create(name='The Pizza Oven')
+        response = self.client.get('/bookings/make_booking')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/make_booking.html')
