f559c90f8b1496e8f542eb6ede0f37916cfa6439
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-09 08:54:47 +0000

    Test the get manage bookings page

--- bookings/test_views.py
+++ bookings/test_views.py
@@ -1,11 +1,17 @@
 """ Testcases for the restaurant app views. """
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 from restaurant.models import Restaurant, Table
 from .models import Booking
 
 
 class TestViews(TestCase):
     """ Tests for the views. """
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_superuser('admin', 'admin@email.com', 'adminpassword')
+
     def test_get_make_booking_page(self):
         """ Test the get make booking page view. """
         Restaurant.objects.create(name='The Pizza Oven')
@@ -24,3 +30,10 @@ class TestViews(TestCase):
         response = self.client.get(f'/bookings/booking_confirmed/{booking.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bookings/booking_confirmed.html')
+
+    def test_get_manage_bookings_page(self):
+        """ Test the get manage bookings page view. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get('/bookings/manage_bookings')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookings/manage_bookings.html')
