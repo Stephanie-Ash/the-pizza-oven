34930d8efbc2d72ef894dbeb9aa66a80b60167a8
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-10 08:27:33 +0000

    Test table search to ensure that the smallest single table is always selected

--- bookings/test_check_availability.py
+++ bookings/test_check_availability.py
@@ -1,3 +1,43 @@
+""" Testcases for the check availability functions """
+import datetime
 from django.test import TestCase
+from restaurant.models import Restaurant, Table
+from .models import Booking
+from .check_availability import find_tables
 
-# Create your tests here.
+
+class TestCheckAvailability(TestCase):
+    """ Tests for the available table searches. """
+    def setUp(self):
+        self.restaurant = Restaurant.objects.create(name='The Pizza Oven')
+        self.table1 = Table.objects.create(restaurant=self.restaurant, size=2)
+        self.table2 = Table.objects.create(restaurant=self.restaurant, size=4)
+        self.table3 = Table.objects.create(restaurant=self.restaurant, size=2)
+        self.table4 = Table.objects.create(restaurant=self.restaurant, size=4)
+        self.table5 = Table.objects.create(restaurant=self.restaurant, size=2)
+        self.table6 = Table.objects.create(restaurant=self.restaurant, size=4)
+
+    def test_smallest_single_table_always_chosen(self):
+        """
+        Test that the smallest available table is chosen when no table
+        combination is required.
+        """
+        selected_table1 = find_tables(
+            datetime.date.today(), datetime.time(18, 00),
+            datetime.time(20, 00), 1, '')
+        self.assertEqual(selected_table1.size, 2)
+
+        selected_table2 = find_tables(
+            datetime.date.today(), datetime.time(18, 00),
+            datetime.time(20, 00), 2, '')
+        self.assertEqual(selected_table2.size, 2)
+
+        selected_table3 = find_tables(
+            datetime.date.today(), datetime.time(18, 00),
+            datetime.time(20, 00), 3, '')
+        self.assertEqual(selected_table3.size, 4)
+
+        selected_table4 = find_tables(
+            datetime.date.today(), datetime.time(18, 00),
+            datetime.time(20, 00), 4, '')
+        self.assertEqual(selected_table4.size, 4)
