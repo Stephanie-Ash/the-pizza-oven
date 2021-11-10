56c1edd6dfb0b62202ec4a18656f6292c66b0ceb
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-11-10 11:35:06 +0000

    Move additional post load js from base.html to separate file in static folder

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // Timeout function to close alert messages after 3 seconds
+    setTimeout(function () {
+        let messages = $("#msg");
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 2000);
+
+    // Modal launch script, url for the confirm delete button taken from data-delete-url attribute
+    // Adapted from https://stackoverflow.com/questions/54806538/
+    $(".manage-delete").click(function () {
+        let del_url = $(this).data('delete-url');
+        $("#manage-delete").attr("href", del_url);
+        $("#booking-delete-modal").modal();
+    });
+});
